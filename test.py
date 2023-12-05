c7f8ba602eda8f6ba7bbe775ce645d96cc86e6b2
khhuisman <karsherohuisman@Karss-MacBook-Air.local>
2023-12-05 18:00:48 +0100

    Area of circle can be calculated.

--- test.py
+++ test.py
@@ -4,3 +4,8 @@ import numpy as np
 
 def circumference_circle(r):
 	return 2*np.pi*r
+
+def area_circle(r):
+	return np.pi*(r**2)
+r0 = 2
+print('The area of a circle of a circle with radius r = {} is {}'.format(r0,area_circle(r0)))
